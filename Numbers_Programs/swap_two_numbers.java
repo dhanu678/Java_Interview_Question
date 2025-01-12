ff0dcff15c415dd996e74dda9a367495a28afb21
dhanashreechavan <dhanashreec8590.com>
2025-01-12 11:26:20 +0530

    Initial commit

---
+++ Numbers_Programs/swap_two_numbers.java
@@ -0,0 +1,10 @@
+class swap_two_numbers{
+    public static void main(String[] args){
+        int a = 4;
+        int b =7;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("after swaping a = " + a + " b = " + b);
+    }
+}
